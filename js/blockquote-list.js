6c94652b1ed1741cba59bdf3ea435240d509d8e3
Krishjot Sarai <93556443+Krishjot8@users.noreply.github.com>
2023-10-27 22:34:57 -0500

    updates

--- js/blockquote-list.js
+++ js/blockquote-list.js
@@ -1,3 +1,13 @@
-function toggleSelected(element) {
-    element.classList.toggle("selected");
-  }
+let currentIndex = 0;
+
+function toggleSelected(index) {
+    const dots = document.querySelectorAll('.dot');
+    const slides = document.querySelector('.reviews-slider');
+    const slideWidth = document.querySelector('blockquote').clientWidth;
+
+    dots[currentIndex].classList.remove('selected');
+    dots[index].classList.add('selected');
+   dots[index].style.backgroundColor = '#1e83ec';
+    slides.style.transform = `translateX(-${index * slideWidth}px)`;
+    currentIndex = index;
+}
