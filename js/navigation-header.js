5c0f14dd52364d56d4172170ad7e009385e08d22
Krishjot Sarai <93556443+Krishjot8@users.noreply.github.com>
2023-11-02 02:10:03 -0500

    fixed margin of our values

--- js/navigation-header.js
+++ js/navigation-header.js
@@ -23,3 +23,38 @@ window.onscroll = function() {
 
   prevScrollPos = currentScrollPos;
 }
+
+let prevScrollPos1 = window.pageYOffset;
+const Hnavbar1 = document.getElementById("navbar");
+
+window.onload = function() {
+  handleScroll(); // Call the function initially to set the initial background color
+}
+
+window.onscroll = function() {
+  handleScroll(); // Call the function on scroll to update the background color
+}
+
+function handleScroll() {
+  const currentScrollPos = window.pageYOffset;
+
+  // Check the screen width
+  const mediaQuery = window.matchMedia('(max-width: 576px)');
+  
+  if (mediaQuery.matches) {
+    // If screen width is at most 576px, set the background color to black
+    Hnavbar.style.backgroundColor = "rgba(0, 0, 0, 1)";
+  } else {
+    if (currentScrollPos === 0) {
+      Hnavbar.style.backgroundColor = "transparent";
+    } else if (currentScrollPos > prevScrollPos) {
+      // Scrolling down
+      Hnavbar.style.backgroundColor = "rgba(0, 0, 0, 1.8)"; // Set the black background when scrolling down
+    } else {
+      // Scrolling up or not moving (handled as scrolling up for simplicity)
+      Hnavbar.style.visibility = 'none'; // Set the background to transparent
+    }
+  }
+
+  prevScrollPos = currentScrollPos;
+}
